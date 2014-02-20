7d01ed3f0136f6a2c87bd5b93c5e6a6839e98334
ellieTycho <elliejmitchell8@gmail.com>
2014-02-20 10:34:55 +0000

    Added Example1

---
+++ Example1.java
@@ -0,0 +1,44 @@
+public Class Example1{
+
+private int changeState(char input, int state){
+
+	int newState;
+	
+	if( state == 0 ){
+	
+		if( input == 'A' ){
+			newState = 3;
+		}		
+		else if( input == 'B' ){
+			newState = 2;		
+		}
+		else{
+			 newState = 1;
+		}		
+	}
+	else if( state == 1 ){
+		
+	    if( input == 'A' ){
+			newState = 2;
+		}		
+		else if( input == 'B' ){
+			newState = 3;		
+		}
+		else{
+			 newState = null;
+		}
+	}
+	else{
+		
+		if( input == 'C' ){
+			newState = 3;
+		}
+		else{
+			newState = null;
+		}
+	}
+	
+	return newState;
+}
+
+}
